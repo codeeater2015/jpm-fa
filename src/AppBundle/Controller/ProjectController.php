332183c59a890ec788e7d2458924d3f249714fa1
Mark Daryl C. De Taza <detazamarkdaryl2014@gmail.com>
2024-05-30 16:03:17 +0800

    updated project DataController

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -370,7 +370,7 @@ class ProjectController extends Controller
     */
 
     public function ajaxSelect2ProjectVoters(Request $request){
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->getDoctrine()->getManager("electionPrep2024");
         $user = $this->get('security.token_storage')->getToken()->getUser();
 
         $electId = $request->get("electId");

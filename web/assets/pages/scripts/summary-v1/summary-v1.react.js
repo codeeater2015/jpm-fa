d862f177ff1fb531ddd3675d8c0e4e7b3913a115
Mark Daryl C. De Taza <detazamarkdaryl2014@gmail.com>
2024-09-17 20:30:48 +0800

    update summary

--- web/assets/pages/scripts/summary-v1/summary-v1.react.js
+++ web/assets/pages/scripts/summary-v1/summary-v1.react.js
@@ -119,9 +119,9 @@ var SummaryV1Component = React.createClass({
                             <SummaryV1Table clusterName="C3" municipalityNo={16} summaryDate={this.state.summaryDate} />
                             <h3><strong>North West Cluster</strong></h3>
                             <SummaryV1Table clusterName="C4" municipalityNo={16} summaryDate={this.state.summaryDate} />
-                            <h3><strong>Poblacion <small>Big Brgy Cluster</small> Cluster</strong></h3>
+                            <h3><strong>Poblacion Big Brgy Cluster</strong></h3>
                             <SummaryV1Table clusterName="C1" municipalityNo={16} summaryDate={this.state.summaryDate} />
-                            <h3><strong>Poblacion <small>(Small Brgy Cluster)</small> Cluster</strong></h3>
+                            <h3><strong>Poblacion Small Brgy Cluster</strong></h3>
                             <SummaryV1Table clusterName="C2" municipalityNo={16} summaryDate={this.state.summaryDate} />
                             <h3><strong>South Cluster</strong></h3>
                             <SummaryV1Table clusterName="C5" municipalityNo={16} summaryDate={this.state.summaryDate} />
